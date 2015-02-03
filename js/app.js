448f9d8551e9cdcfa8a06b6d51b2c95b1f8ad23e
jimmypchen <jimmy.pk.chen@gmail.com>
2015-02-02 21:38:03 -0800

    test
    just replacing a few things, not fully working yet

---
+++ js/app.js
@@ -0,0 +1,37 @@
+var quizApp = angular.module('quiz', ['ngRoute']);
+
+quizApp.config(function($routeProvider) {
+  //set up routes
+  $routeProvider
+    .when('/', {
+      controller: 'QuizController',
+      templateUrl: 'app_pages/main.html'
+    })
+
+    .when('/type', {
+      templateUrl: 'app_pages/quizA_start.html'
+    })
+
+    .when('/q1', {
+      controller: 'QuizAController',
+      templateUrl: 'app_pages/quizA_1.html'
+    })
+
+    .when('/result', {
+      controller: 'QuizController',
+      templateUrl: 'app_pages/quizA_result.html'
+    })
+
+    .otherwise( {
+    	redirectTo: '/'
+    })
+});
+
+
+quizApp.controller('QuizController', function($scope) {
+  $scope.quizData = {};
+});
+
+quizApp.controller('QuizAController', function($scope) {
+  $scope.quizData = {};
+});
--- js/main.js
+++ js/main.js
@@ -1,21 +1,5 @@
-var quizApp = angular.module('quiz', ['ngRoute']);
+angular.module('quiz')
 
-quizApp.config(function($routeProvider) {
-  //set up routes
-  $routeProvider
-    .when('/type', {
-      templateUrl: 'app_pages/quizA_start.html'
-    })
-
-    .when('/q1', {
-      templateUrl: 'app_pages/quizA_1.html'
-    })
-
-    .when('/result', {
-      templateUrl: 'app_pages/quizA_result.html'
-    })
-
-    .otherwise( {
-    	templateUrl: 'app_pages/main.html'
-    })
-});
+.controller('quizController', function($scope) {
+  $scope.quizData = {};
+}
