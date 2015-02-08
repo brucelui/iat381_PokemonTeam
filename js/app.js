d0c46a379a7b5d3fcf12bef3531da7c7f8047396
Bruce <lui.bruce@hotmail.com>
2015-02-07 17:43:05 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -24,27 +24,3 @@ pokeApp.config(['$routeProvider',
     	templateUrl: 'app_pages/main.html'
     });
 }]);
-
-pokeApp.controller('pokeController', function($scope) {
-  /* var questions = {
-    q1: {
-        question: "WHAT WOULD YOU USE TO PROTECT YOURSELF?",
-    options: ["HARDEN", "WITHDRAW", "REFLECT", "DETECT"],
-    },
-    q2: {
-      question: "WHICH ATTACK SUITS YOU BEST?",
-      options: ["FLAMETHROWER", "HYDRO PUMP", "SOLARBEAM", "SPLASH"],
-      nextUrl: '#quiz/q3',
-    },
-  } */
-
-  $scope.answerValue = 0;
-  $scope.questionValue = 0;
-
-  $scope.updateOutput = function (btn) {
-        $scope.answerValue += btn;
-        $scope.questionValue += 1;
-    };
-
- });
-
